9b8c1eeb4d5b5036823cfda8d33033c640a7aa61
ajavierfm7 <ajavierfm7@gmail.com>
2025-03-08 16:29:45 -0600

    Se agrega proyecto

---
+++ app/src/androidTest/java/org/ajavierfm7/sesion8m/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package org.ajavierfm7.sesion8m
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("org.ajavierfm7.sesion8m", appContext.packageName)
+    }
+}
